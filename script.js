95b2becd32c2d31cf678adfb61c6139dde42dbb3
zurgboy2 <165296816+zurgboy2@users.noreply.github.com>
2024-07-02 11:37:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', fetchCourses);
 
 function fetchCourses() {
   const script = document.createElement('script');
-  script.src = 'YOUR_GOOGLE_APP_SCRIPT_WEB_APP_URL?callback=displayCourses';
+  script.src = 'https://script.google.com/macros/s/AKfycbzoZCalGB_Zvxe0eIoF-XTO-Oh2d1HiQ8dr0FYc4adF3_L_9MzFLH34czV0O_aHmtTW0Q/exec?callback=displayCourses';
   document.body.appendChild(script);
 }
 
